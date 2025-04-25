d27db789f672b6e079c4a698fe09b9db1bea809f
Diego <diegofoo59@mail.com>
2025-04-25 07:22:01 -0500

    a

--- gotypes/doc/main.go
+++ gotypes/doc/main.go
@@ -50,7 +50,9 @@ func main() {
 	// Walk up to the enclosing ast.Decl for the doc comment.
 	for _, file := range pkg.Syntax {
 		pos := obj.Pos()
-		if !(file.FileStart <= pos && pos < file.FileEnd) {
+		start := file.Pos()
+		end := file.End()
+		if !(start <= pos && pos < end) {
 			continue // not in this file
 		}
 		path, _ := astutil.PathEnclosingInterval(file, pos, pos)
@@ -67,23 +69,3 @@ func main() {
 	}
 	//!-part2
 }
-
-// (The $GOROOT below is the actual string that appears in file names
-// loaded from export data for packages in the standard library.)
-
-/*
-//!+output
-$ ./doc net/http File
-type net/http.File interface{Readdir(count int) ([]os.FileInfo, error); Seek(offset int64, whence int) (int64, error); Stat() (os.FileInfo, error); io.Closer; io.Reader}
-$GOROOT/src/io/io.go:92:2: method (net/http.File) Close() error
-$GOROOT/src/io/io.go:71:2: method (net/http.File) Read(p []byte) (n int, err error)
-/go/src/net/http/fs.go:65:2: method (net/http.File) Readdir(count int) ([]os.FileInfo, error)
-$GOROOT/src/net/http/fs.go:66:2: method (net/http.File) Seek(offset int64, whence int) (int64, error)
-/go/src/net/http/fs.go:67:2: method (net/http.File) Stat() (os.FileInfo, error)
-
- A File is returned by a FileSystem's Open method and can be
-served by the FileServer implementation.
-
-The methods should behave the same as those on an *os.File.
-//!-output
-*/
